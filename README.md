906ad5c35202abbc14481523c771a0498bfcb6b4
Hugo Wainwright <wainwrighthugo@gmail.com>
2018-06-21 00:22:49 +0100

    implemented scripts

---
+++ README.md
@@ -0,0 +1,26 @@
+OBS Studio SCII Scene Switcher
+==============================
+
+Installation Instructions
+-------------------------
+
+- Extract _OBS Studio SCII Scene Switcher_ into a directory of your
+  choosing.
+- Inside OBS Studio, navigate to 'Tools' -> 'Scripts'.
+- Switch to the 'Python Settings' tab, then set the 'Python Install
+  Path' to the `python36` directory inside the directory you extracted
+  _OBS Studio SCII Scene Switcher_ to.
+- Switch back to the 'Scripts' tab, then click the '+' button to add a
+  new script.
+- Navigate to the directory you extracted _OBS Studio SCII Scene
+  Switcher_ to, then select the file `sc2_scene_switcher.py`
+- The script should now be enabled and ready to use!
+
+Usage Instructions
+------------------
+
+- Inside OBS Studio, create two scenes: one named `sc2_switcher_in_game`
+  and the other named `sc2_switcher_out_of_game`.
+- While the script is enabled, the former scene will be set as the
+  active scene when StarCraft II enters a game, and the latter scene
+  will be set as the active scene when StarCraft II exits a game.
--- sc2_scene_switcher.py
+++ sc2_scene_switcher.py
@@ -1,4 +1,9 @@
 import obspython as obs
+import urllib.request
+import json
+
+IN_GAME = 'sc2_switcher_in_game'
+OUT_OF_GAME = 'sc2_switcher_out_of_game'
 
 
 def find_source_by_name_in_list(source_list, name):
@@ -13,15 +18,34 @@ def set_scene_by_name(scene_name):
     scenes = obs.obs_frontend_get_scenes()
 
     scene = find_source_by_name_in_list(scenes, scene_name)
-    obs.obs_frontend_set_current_scene(scene)
+    if scene is not None:
+        obs.obs_frontend_set_current_scene(scene)
 
     obs.source_list_release(scenes)
 
 
+def is_in_game():
+    with urllib.request.urlopen("http://localhost:6119/ui") as response:
+        ui_data = json.load(response)
+    num_active_screens = len(ui_data.get('activeScreens', []))
+    with urllib.request.urlopen("http://localhost:6119/game") as response:
+        game_data = json.load(response)
+    display_time = game_data.get("displayTime", 0.0)
+
+    return num_active_screens == 0 and display_time > 0.0
+
+
+def switch_scene():
+    try:
+        scene_name = IN_GAME if is_in_game() else OUT_OF_GAME
+        set_scene_by_name(scene_name)
+    except:
+        pass
+
+
 def script_load(settings):
-    set_scene_by_name("blah2")
-    pass
+    obs.timer_add(switch_scene, 500)
 
 
 def script_unload():
-    pass
+    obs.timer_remove(switch_scene)
