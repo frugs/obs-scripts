ed467bb678c4a42847926744f3cc6c38c504105d
Hugo Wainwright <hugowainwright@fb.com>
2018-06-21 15:27:20 +0100

    Fix bugs

--- sc2_scene_switcher.py
+++ sc2_scene_switcher.py
@@ -7,6 +7,7 @@ import asyncio
 IN_GAME = 'sc2_switcher_in_game'
 OUT_OF_GAME = 'sc2_switcher_out_of_game'
 
+event_loop = asyncio.new_event_loop()
 event_loop_thread = None
 prev_in_game = None
 
@@ -44,25 +45,23 @@ def is_in_game():
 
 
 def run_event_loop():
-    asyncio.get_event_loop().run_forever()
+    event_loop.run_forever()
 
 
 async def stop_event_loop():
-    asyncio.get_event_loop().stop()
+    event_loop.stop()
 
 
 async def switch_scene():
-    cur_in_game = await asyncio.get_event_loop().run_in_executor(
-        None, is_in_game())
+    cur_in_game = await event_loop.run_in_executor(None, is_in_game())
     if cur_in_game is not None and (prev_in_game is None
                                     or cur_in_game != prev_in_game):
         scene_name = IN_GAME if cur_in_game else OUT_OF_GAME
-        await asyncio.get_event_loop().run_in_executor(
-            None, set_scene_by_name(scene_name))
+        await event_loop.run_in_executor(None, set_scene_by_name(scene_name))
 
 
 def queue_switch_scene():
-    asyncio.get_event_loop().call_soon_threadsafe(switch_scene())
+    asyncio.run_coroutine_threadsafe(switch_scene(), event_loop)
 
 
 def script_load(settings):
@@ -73,7 +72,6 @@ def script_load(settings):
 
 def script_unload():
     obs.timer_remove(queue_switch_scene)
-    future = asyncio.run_coroutine_threadsafe(stop_event_loop(),
-                                              asyncio.get_event_loop())
+    future = asyncio.run_coroutine_threadsafe(stop_event_loop(), event_loop)
     future.result(2)
     event_loop_thread.join(2)
