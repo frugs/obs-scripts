77629e75680b6c67657ac9ab9ea38617e2a651c4
Hugo Wainwright <hugowainwright@fb.com>
2018-06-21 15:54:42 +0100

    fix bug

--- sc2_scene_switcher.py
+++ sc2_scene_switcher.py
@@ -56,6 +56,7 @@ async def switch_scene():
     cur_in_game = await event_loop.run_in_executor(None, is_in_game())
     if cur_in_game is not None and (prev_in_game is None
                                     or cur_in_game != prev_in_game):
+        prev_in_game = cur_in_game
         scene_name = IN_GAME if cur_in_game else OUT_OF_GAME
         await event_loop.run_in_executor(None, set_scene_by_name(scene_name))
 
